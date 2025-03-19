f67537265f428e11dd23ddf300ac94e7f8991eff
ggLoow <joaoponteixeira@gmail.com>
2025-03-19 13:59:00 +0000

    Collect the collectible
    Programming Essentials

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float RotatingSpeed;
+    public GameObject onCollectEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Rotate(0, RotatingSpeed, 0);
+        transform.Rotate(0, RotatingSpeed, 0);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+
+
+    // Destroy the collectible
+    Destroy(gameObject);
+    Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
     }
 }
